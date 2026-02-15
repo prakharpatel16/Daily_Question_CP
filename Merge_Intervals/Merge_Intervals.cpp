9a9a0430f484336fda7d994c4f947fa59d6dc8b5
Prakhar Patel <07patelprakhar@gmail.com>
2026-02-15 23:01:30 +0530

    Add files via upload

---
+++ Merge_Intervals/Merge_Intervals.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(intervals.begin(),intervals.end());
+
+    vector<vector<int>> ans;
+
+    for(int i=0;i<intervals.size();i++){
+        if(ans.empty() or intervals[i][0] > ans.back()[1]){
+            ans.push_back(intervals[i]);
+        }
+        else{
+            ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+        }
+    }
+    return ans;
+    }
+};
