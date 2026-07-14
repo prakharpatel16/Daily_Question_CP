20bc72820230de5cc4a12a03b5db3097ae295613
Prakhar Patel <07patelprakhar@gmail.com>
2026-07-14 14:44:10 +0530

    File updated at 14/07/2026, 14:44:09

--- 3336-FindtheNumberofSubsequencesWithEqualGCD/3336-FindtheNumberofSubsequencesWithEqualGCD.cpp
+++ 3336-FindtheNumberofSubsequencesWithEqualGCD/3336-FindtheNumberofSubsequencesWithEqualGCD.cpp
@@ -1,24 +1,27 @@
-// Last updated: 14/07/2026, 13:58:05
+// Last updated: 14/07/2026, 14:44:09
 1class Solution {
 2public:
-3    int dp[201][201][201];
-4    const int MOD = 1e9 + 7;
-5    int solve(vector<int> &nums,int i, int first, int second){
-6        if(i == nums.size()){
-7            bool bothNonEmpty = (first != 0) and (second != 0);
-8            bool isMatched = (first == second);
-9            return (bothNonEmpty and isMatched) ? 1 : 0;
-10        }
-11        if(dp[i][first][second] != -1){
-12            return dp[i][first][second];
+3    const int MOD = 1e9 + 7;
+4    int subsequencePairCount(vector<int>& nums) {
+5        int maxi = *max_element(nums.begin(),nums.end());
+6        int dp[nums.size()+1][maxi+1][maxi+1];
+7        for(int first=0;first<=maxi;first++){
+8            for(int second=0;second<=maxi;second++){
+9                int bothNonEmpty = (first != 0) and (second != 0);
+10                int GcdMatched = (first == second);
+11                dp[nums.size()][first][second] = (bothNonEmpty and GcdMatched) ? 1 : 0;
+12            }
 13        }
-14        int skip = solve(nums,i+1,first,second);
-15        int take1 = solve(nums,i+1,__gcd(first,nums[i]),second);
-16        int take2 = solve(nums,i+1,first,__gcd(second,nums[i]));
-17        return dp[i][first][second] = (1LL* skip + take1 + take2) % MOD;
-18    }
-19    int subsequencePairCount(vector<int>& nums) {
-20        memset(dp,-1,sizeof(dp));
-21        return solve(nums,0,0,0);      
-22    }
-23};
+14        for(int i=nums.size()-1;i>=0;i--){
+15            for(int first=maxi;first>=0;first--){
+16                for(int second=maxi;second>=0;second--){
+17                    int skip = dp[i+1][first][second];
+18                    int take1 = dp[i+1][__gcd(first,nums[i])][second];
+19                    int take2 = dp[i+1][first][__gcd(second,nums[i])]; 
+20                    dp[i][first][second] = (1LL * skip + take1 + take2) % MOD;
+21                }
+22            }
+23        }
+24        return dp[0][0][0];
+25    }
+26};
